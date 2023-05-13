43e03efc5befe2913013d412c1f27755a88fc0c6
Adrian <karpinski.adrian1@gmail.com>
2023-05-13 18:57:27 +0200

    Fueltank
    Added fueltank class

---
+++ Fueltank.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSim
+{
+    public class Fueltank
+    {
+        private const int Capacity = 80;
+        private int currentFuel;
+
+        public Fueltank()
+        {
+            currentFuel = 0;
+        }
+    }
+}
